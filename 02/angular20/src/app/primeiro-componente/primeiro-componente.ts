cd50a746c8a0f9c963a5d5487b64f70720c314eb
GersonESS <gebhsantos@gmail.com>
2025-12-07 18:54:13 -0300

    a

--- 02/angular20/src/app/primeiro-componente/primeiro-componente.ts
+++ 02/angular20/src/app/primeiro-componente/primeiro-componente.ts
@@ -6,6 +6,29 @@ import { Component } from '@angular/core';
   templateUrl: './primeiro-componente.html',
   styleUrl: './primeiro-componente.css'
 })
-export class PrimeiroComponente {
+export class PrimeiroComponente 
+{
+  nome:string = 'Angular';
+  // multiplicarValor(a:number, b:number):number 
+  // {
+  //   return valor * 2;
+  // }
+  idade:number = 10;
+  mensagem():string 
+  {
+    return 'Seja bem-vindo ao Angular!'   
 
-}
+  };
+  media:number = 6.5;
+  // media(n1:number, n2:number, n3:number):number 
+  // {
+  //   return (n1 + n2 + n3) / 3;   
+  // }
+  pessoa =
+  {
+    nome: 'João',
+    idade: 30,
+    cidade: 'São Paulo'
+  }
+};
+ 
